a0d24239e18a218346af63c1ce0bf38bd1878a02
Vinícius A. Haga <viniciushaga@outlook.com>
2024-06-01 16:11:54 -0300

    Docker configuration

--- WoodCatalog/WoodCatalog.API/Program.cs
+++ WoodCatalog/WoodCatalog.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
+using System.Text;
 using WoodCatalog.Domain.Repositories.Interfaces;
 using WoodCatalog.Domain.Services;
 using WoodCatalog.Domain.Services.Interfaces;
@@ -13,11 +16,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#if DEBUG
 builder.Services.AddSingleton<IConnectionMultiplexer>(options =>
-    ConnectionMultiplexer.Connect("127.0.0.1:6379,abortConnect=false "));
-    //ConnectionMultiplexer.Connect("redis_image"));
+    ConnectionMultiplexer.Connect("127.0.0.1:6379,abortConnect=false"));
+#else
+builder.Services.AddSingleton<IConnectionMultiplexer>(options =>
+    ConnectionMultiplexer.Connect("redis:6379,abortConnect=false")); 
+#endif
+
+
 builder.Services.AddScoped<IWoodRepository, WoodRepository>();
 builder.Services.AddScoped<IWoodService, WoodService>();
+
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
+builder.Services.AddAuthentication(cfg => {
+    cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = false;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8
+            .GetBytes(configuration["ApplicationSettings:JWT_Secret"]!)
+        ),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 var app = builder.Build();
 
@@ -29,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
