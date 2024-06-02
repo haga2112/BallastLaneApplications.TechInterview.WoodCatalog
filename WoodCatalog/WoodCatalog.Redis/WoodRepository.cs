8328dcd5953227df27aae716d72acee3b806b821
Vinícius A. Haga <viniciushaga@outlook.com>
2024-06-02 18:37:48 -0300

    Update WoodRepository.cs

--- WoodCatalog/WoodCatalog.Redis/WoodRepository.cs
+++ WoodCatalog/WoodCatalog.Redis/WoodRepository.cs
@@ -1,10 +1,5 @@
 ﻿using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using WoodCatalog.Domain.Models;
 using WoodCatalog.Domain.Repositories.Interfaces;
 
